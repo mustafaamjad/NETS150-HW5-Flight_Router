db7785e5a8600edeb1942a5968c2df41f29bd73e
Shantanu Puri <shantanupuri26@levyvlan502.0170.wlan.wireless-resnet.upenn.edu>
2017-05-05 17:56:46 -0400

    graph set up

--- FlightRouter.java
+++ FlightRouter.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 
 public class FlightRouter {
 
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		FlightGraph graph = new FlightGraph("airports.csv", "routes.csv");
-		
-		// GraphAlgorithms test = new GraphAlgorithms(graph);
-		System.out.println("Hello World"); 
+
+		// TEST doesEdgeExist 
+		System.out.println(graph.doesEdgeExist("YVR", "DEN"));
 	}
 }
--- NodeInterface.java
+++ NodeInterface.java
@@ -1,4 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public interface NodeInterface {
 
-	//TODO: add functions here
+	public String getName();
+
+	public String getIataCode();
+
+	public Double getGeoLat();
+
+	public Double getGeoLong();
+
+	public boolean addOutAirport(String newOutAirport);
+
+	public boolean addInAirport(String newInAirport);
+
+	public ArrayList<String> getInAirports();
+
+	public ArrayList<String> getOutAirports();
 }
