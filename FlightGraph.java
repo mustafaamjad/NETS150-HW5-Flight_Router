f4bb075549f34ea8e618115bf9e5f72b641cdce1
Shantanu Puri <shantanupuri26@levyvlan502.0170.wlan.wireless-resnet.upenn.edu>
2017-05-05 06:03:23 -0400

    same page

--- FlightGraph.java
+++ FlightGraph.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
 public class FlightGraph implements FlightGraphInterface {
+
+	private HashMap<Node, ArrayList<Node>> in = new HashMap<Node, ArrayList<Node>>(); 
 	
 	public FlightGraph(String airports, String routes) {
+
 	}
 
 
@@ -8,11 +12,11 @@ public class FlightGraph implements FlightGraphInterface {
 		return false;
 	}
 	
-	private boolean addNode(String name, String iataCode, double geoLong, double geoLat) {
-		return false;
-	}
+	// private boolean addNode(String name, String iataCode, double geoLong, double geoLat) {
+	// 	return false;
+	// }
 	
-	private boolean addEdge(Node m, Node n){
-		return false;
-	}
+	// private boolean addEdge(Node m, Node n){
+	// 	return false;
+	// }
 }
