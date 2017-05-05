bcadc59353f54e7a6e4b6d447356b6a5b829d5f3
mustafaamjad <mamjad@sas.upenn.edu>
2017-05-05 19:08:48 -0400

    updated FlightGraphInterface

--- FlightGraphInterface.java
+++ FlightGraphInterface.java
@@ -2,4 +2,8 @@ public interface FlightGraphInterface {
 
 	public boolean doesEdgeExist(String m, String n);
 
+	public void getInAndOutFlights(String currentAirport);
+	
+	public void getAllInAndOutFlights();
+
 }
