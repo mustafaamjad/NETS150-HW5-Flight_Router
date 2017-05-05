bd08db62e219815674be0af20276eacf97dae3ca
mustafaamjad <mamjad@sas.upenn.edu>
2017-05-05 06:03:34 -0400

    updated FlightGraph to retrieve airport info

--- FlightGraph.java
+++ FlightGraph.java
@@ -1,6 +1,47 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class FlightGraph implements FlightGraphInterface {
+
+	private List<Node> allAirports = new ArrayList<Node>();
 	
-	public FlightGraph(String airports, String routes) {
+	public FlightGraph(String airports, String routes) throws IOException {
+
+		// Parses the file containing airport data and creates nodes
+		// for each airport
+		Reader r = new FileReader(airports);
+		BufferedReader br = new BufferedReader(r);
+		String line;
+
+		while( (line = br.readLine()) != null) {
+			// System.out.println("while loop for creating nodes");
+			// System.out.println(line);
+			String[] airportData = line.split(",");
+			String airportName = airportData[1];
+			String airportIataCode = airportData[4];
+			double airportGeoLat = Double.parseDouble(airportData[6]);
+			double airportGeoLong = Double.parseDouble(airportData[7]);
+			Node newAirport = new Node(airportName, airportIataCode, 
+				airportGeoLat, airportGeoLong);
+			allAirports.add(newAirport);
+		}
+
+		r.close();
+		printNodes();
+	}
+
+	private void printNodes() {
+		for (Node elt : allAirports) {
+			System.out.println("This airport has name: " + elt.name +
+				" and IATA code: " + elt.iataCode + " and latitude: " + 
+				elt.geoLat + "and longitude: " + elt.geoLong);
+		}
 	}
 
 
@@ -8,7 +49,7 @@ public class FlightGraph implements FlightGraphInterface {
 		return false;
 	}
 	
-	private boolean addNode(String name, String iataCode, double geoLong, double geoLat) {
+	private boolean addNode(String name, String iataCode, double geoLat, double geoLong) {
 		return false;
 	}
 	
--- FlightRouter.java
+++ FlightRouter.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
+
 public class FlightRouter {
 
-	public static void main(String[] args) {
-		FlightGraph graph = new FlightGraph("");
+	public static void main (String[] args) throws IOException {
+		FlightGraph graph = new FlightGraph("airports.csv", "Hello");
 		GraphAlgorithms test = new GraphAlgorithms(graph);
 		System.out.println("Hello World"); 
 	}
