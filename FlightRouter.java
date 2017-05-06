7ab1138e9e5913282b2e6bece32ef08144b807d5
mustafaamjad <mamjad@sas.upenn.edu>
2017-05-05 22:11:06 -0400

    distance algorithm

--- FlightRouter.java
+++ FlightRouter.java
@@ -4,9 +4,10 @@ public class FlightRouter {
 
 	public static void main(String[] args) throws IOException {
 		FlightGraph graph = new FlightGraph("airports.csv", "routes.csv");
+		// System.out.println(GraphAlgorithms.calculateDistance(31.52160072, 74.4036026, 33.61669922, 73.09919739));
 
 		// TEST doesEdgeExist 
-		// System.out.println(graph.doesEdgeExist("YVR", "DEN"));
+		System.out.println(graph.doesEdgeExist("YVR", "DEN"));
 		graph.getAllInAndOutFlights();
 	}
 }
